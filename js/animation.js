5db0f4d1617a765e3d04f86a4ab9abf66387629d
benjaminleeschnell <benjaminleeschnell@gmail.com>
2015-12-17 21:04:03 -0500

    added bouncing image and 1-5 pages

--- js/animation.js
+++ js/animation.js
@@ -19,7 +19,13 @@ $(document).ready(function() {
     var radius = 90; 						/* Radius of circels.        */
     var speed = 2;    						/* Speed of circles.         */
     var textColor = 'rgb(255,255,255)';		/* Color of text in circles. */
-    var image = document.getElementById("source");
+    var image0 = document.getElementById("source0");
+    var image1 = document.getElementById("source1");
+    var image2 = document.getElementById("source2");
+    var image3 = document.getElementById("source3");
+    var image4 = document.getElementById("source4");
+    var image5 = document.getElementById("source5");
+    var image6 = document.getElementById("source6");
     /* ///////////////////////////////////////////////////////////////// */
 
 
@@ -96,11 +102,74 @@ $(document).ready(function() {
 
 	var circles = new Array(); /* An array containing instances of the object class. */
 	circles[0] = new Circle('rgb(204,0,1)', 150, 150, "./k.html", "K", "80px Arial Black", -33, 30);
+    
+     circles[0].draw = function() {
+      	context.beginPath();
+        context.arc(Math.floor(this.R[0]), Math.floor(this.R[1]), this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
+        context.fill();
+        context.drawImage(image0, this.R[0] -91, this.R[1] -90, this.radius + 90, this.radius + 90);
+  
+    };
+
     circles[1] = new Circle('rgb(241, 92, 0)', 400, 150, "./grade-1.html", "1", "80px Arial Black", -30, 30);
+   
+     circles[1].draw = function() {
+      	context.beginPath();
+        context.arc(Math.floor(this.R[0]), Math.floor(this.R[1]), this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
+        context.fill();
+        context.drawImage(image1, this.R[0] -91, this.R[1] -90, this.radius + 90, this.radius + 90);
+  
+    };
+
     circles[2] = new Circle('rgb(255, 232, 0)', 650, 150, "./grade-2.html", "2", "80px Arial Black", -30, 30);
+    
+     circles[2].draw = function() {
+      	context.beginPath();
+        context.arc(Math.floor(this.R[0]), Math.floor(this.R[1]), this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
+        context.fill();
+        context.drawImage(image2, this.R[0] -91, this.R[1] -90, this.radius + 90, this.radius + 90);
+  
+    };
+
     circles[3] = new Circle('rgb(0, 153, 0)', 150, 400, "./grade-3.html", "3", "80px Arial Black", -30, 30);
+    
+      circles[3].draw = function() {
+      	context.beginPath();
+        context.arc(Math.floor(this.R[0]), Math.floor(this.R[1]), this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
+        context.fill();
+        context.drawImage(image3, this.R[0] -91, this.R[1] -90, this.radius + 90, this.radius + 90);
+  
+    };
+
     circles[4] = new Circle('rgb(88, 214, 226)', 350, 400, "./grade-4.html", "4", "80px Arial Black", -30, 30);
+   
+      circles[4].draw = function() {
+      	context.beginPath();
+        context.arc(Math.floor(this.R[0]), Math.floor(this.R[1]), this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
+        context.fill();
+        context.drawImage(image4, this.R[0] -91, this.R[1] -90, this.radius + 90, this.radius + 90);
+  
+    }; 
+
     circles[5] = new Circle('rgb(0, 102, 204)', 550, 400, "./grade-5.html", "5", "80px Arial Black", -30, 30);
+   
+
+       circles[5].draw = function() {
+      	context.beginPath();
+        context.arc(Math.floor(this.R[0]), Math.floor(this.R[1]), this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
+        context.fill();
+        context.drawImage(image5, this.R[0] -91, this.R[1] -90, this.radius + 90, this.radius + 90);
+  
+    }; 
+
+
+
     circles[6] = new Circle('rgb(125, 0, 204)', 750, 400, "./#", "?", "80px Arial Black", -30, 33);
 
 
@@ -109,13 +178,9 @@ $(document).ready(function() {
         context.arc(Math.floor(this.R[0]), Math.floor(this.R[1]), this.radius, 0, 2 * Math.PI, false);
         context.fillStyle = this.color;
         context.fill();
-
-    //      context.font = "50px Arial Black";
-    //      context.fillStyle = textColor;
-          context.drawImage(image, this.R[0] -90, this.R[1] -90, this.radius + 90, this.radius + 90);
-    //     context.fillText("School", this.R[0] - 95, this.R[1] + 45)
-    }; /* The High School circle needed two lines of text, however, canvas does not support line break so I had 
-    	  //to get creative and change the value of .draw() for the high school circle. */
+        context.drawImage(image6, this.R[0] -90, this.R[1] -90, this.radius + 90, this.radius + 90);
+  
+    }; 
 
 
     var wallCheck = function(e) {
